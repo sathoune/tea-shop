956059fe26b15ba28f855bac559990dd82350889
John Doe <michal5bargiel@gmail.com>
2019-01-27 14:59:37 +0000

    Developed script to create Item representation

--- scripts/createItemDiv.js
+++ scripts/createItemDiv.js
@@ -12,8 +12,27 @@ function createItem(){
 }
 
 function createItemDiv(item_id){
-    var div = `<div id=${item_id}><input type='text'></div>`;     
+    var div = `<div id=${item_id}></div>`;     
     
     $("body").append(div);  
+    insertInputsInto($(`#${item_id}`));
     
-};
+}
+
+function insertInputsInto(div){
+    var nameInput = '<input type="text" class="name" list="tees">'
+    var codeInput = '<input type="text" class="registerCode">';
+    var priceInput = '<input class="price" type="text">';
+    var quantityInput = '<input class="quantity" type="number" name="quantity" min="0" value="1">';
+    var typeInput = '<select class="type">\
+                <option value="sztuka">sztuka</option>\
+                <option value="czajnik">czajnik</option>\
+                <option value="gaiwan">gaiwan</option>\
+                <option value="opakowanie">opakowanie</option>\
+                <option value="gram">gram</option>\
+                </select>';
+    div.append(codeInput, nameInput, typeInput, quantityInput, priceInput);
+    
+}
+
+        
