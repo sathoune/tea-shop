7aa02427355e20b064dd8276b0c77aad0957d166
John Doe <michal5bargiel@gmail.com>
2019-01-28 00:16:29 +0000

    Created labels in UI-order

--- scripts/createOrder.js
+++ scripts/createOrder.js
@@ -17,8 +17,26 @@ function createOrderDiv(order_id, itemsQuantity=4){
     var div = `<div id=${order_id} class='order'></div>`;     
     
     $("#master").append(div);
+    createOrderLabels(order_id);
     for(var i=0; i<itemsQuantity; i++){
         createItem(order_id);
     }
+    
+}
+
+function createOrderFrontPanel(order_id){
+    var orderSelector = `#${order_id}.order`;
+    
+}
+
+function createOrderLabels(order_id){
+  
+    var orderSelector = `#${order_id}.order`;
+    var labelsDiv = `<div class="labels"></div>`;
+    var labelsDivSelector = orderSelector + " .labels";
+      console.log(labelsDiv);
+    $(orderSelector).append(labelsDiv);
+    var labels = `<span>Code </span><span>Name </span><span>Type </span><span>Quantity </span><span>Price </span>`
+    $(labelsDivSelector).append(labels);
     
 }
