4ff2f41ca853cd0d6fdfe3df8bd7ba2250d0bb3b
John Doe <michal5bargiel@gmail.com>
2019-02-11 13:35:56 +0000

    Created public login and register

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,22 @@ var express = require("express");
 var router = express.Router({ mergeParams: true });
 var MenuItem = require("../models/menu");
 var middleware = require("../middleware");
+var passport = require("passport");
+var User = require("../models/user");
 
 
 router.get("/login", (req, res) => {
-   res.send("login pls"); 
+   res.render("login"); 
 });
 
 router.get("/register", (req, res) => {
-   res.send("register here"); 
+   res.render("register"); 
 });
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}), (req, res) => {});
 
 router.get("/", middleware.isLoggedIn, (req, res) => {
     
@@ -19,6 +26,18 @@ router.get("/", middleware.isLoggedIn, (req, res) => {
         else { res.render("index", {foundItems: results}); }
     });
 });
+
+router.post("/register", (req, res) => {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, (err, user) => {
+        if(err){ return res.render("register", { "error": err.message }); }
+        passport.authenticate("local")(req, res, function() {
+            //req.flash("success", "Welcome to YelpCamp," + user.username + "!");
+            res.redirect("/");
+        });
+    });
+});
+
 
 router.get("/:x", middleware.isLoggedIn, (req, res) => {
     res.redirect("/");
