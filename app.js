c09aafa50beab2d69b15bc666b797083fcfaaa40
John Doe <michal5bargiel@gmail.com>
2019-02-14 12:48:29 +0000

    Prepeared for deployment

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(express.static(__dirname + "/public"));
 // connect to db
 const   mongoose    = require("mongoose"),
         dbURL       = process.env.DATABASEURL || "mongodb://localhost:27017/tea-shop";
-
-mongoose.connect(dbURL, {useNewUrlParser: true}, function(err){
+mongoose.connect(dbURL, {useNewUrlParser: true}, (err) => {
     if(err){
         console.log("Something went wrong");
         console.log(err);
@@ -55,7 +54,7 @@ app.use("/",                indexRoutes);
 
 // menu seed
 var seedMenu = require("./db_seeds/seedMenuItems");
-seedMenu();
+//seedMenu();
 
 // server start
-app.listen(process.env.PORT, process.env.IP, () => { console.log("The tea-shop server is on"); });
+app.listen(process.env.PORT || 3000, process.env.IP, () => { console.log("The tea-shop server is on"); });
--- scripts/header.js
+++ scripts/header.js
@@ -11,10 +11,12 @@ function createHeader(){
     const   showMenu            = `<button id='show-menu' class='navigation-button'>
     <i class="fas fa-book-open"></i> Menu <i class="fas fa-book-open"></i></button>`,
             showArchive         = `<button id='show-archive' class='navigation-button'>
-            <i class="fas fa-archive"></i> Archiwum X <i class="fas fa-archive"></i></button>`;
-    const   headerNavigation    = [showMenu, showArchive];
+            <i class="fas fa-archive"></i> Archiwum X <i class="fas fa-archive"></i></button>`,
+            version = "<label id='version'>v. anji-bai-cha</label>";
+    const   headerNavigation    = [version, showMenu, showArchive];
     const   headerDiv           = "<div id='header'></div>";
     $('body').prepend(headerDiv);
+    //$('body').prepend(version);
     $('#header').append(headerNavigation);
     $("#show-menu").on("click", openMenu);
     $("#show-archive").on("click", openArchive);
