b2e944d05a463c41cd0b58579eed9660e7934978
John Doe <michal5bargiel@gmail.com>
2019-02-20 18:31:16 +0000

    Now clicking table input when order is collapsed will open it

--- scripts/order.js
+++ scripts/order.js
@@ -3,9 +3,11 @@ function collapseItems(orderId){
     $(`#${orderId}.order .labels`).toggleClass('hidden');
     if($(`#${orderId}.order .item-container`).hasClass('hidden')){
         $(`#${orderId}.order .collapse-order`).html('<i class="fas fa-angle-down"></i> Pokaż zamówienie <i class="fas fa-angle-down"></i>');
+        $(`#${orderId}.order .table`).on("click", (event) => {collapseItems($(event.target).parent().parent()[0].id)});
         $(`#${orderId}.order .collapse-order`).toggleClass('cosmic-fusion-up');
         $(`#${orderId}.order .collapse-order`).toggleClass('cosmic-fusion-down');
     } else {
+        $(`#${orderId}.order .table`).off("click");
         $(`#${orderId}.order .collapse-order`).html('<i class="fas fa-angle-up"></i> Zwiń zamówienie <i class="fas fa-angle-up"></i>');
         $(`#${orderId}.order .collapse-order`).toggleClass('cosmic-fusion-up');
         $(`#${orderId}.order .collapse-order`).toggleClass('cosmic-fusion-down');
