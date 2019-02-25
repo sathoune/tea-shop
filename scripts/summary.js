4adc14cd5f61d9b1bf9394074974f9097e1a3512
John Doe <michal5bargiel@gmail.com>
2019-02-25 21:33:50 +0000

    User can sort by quantity or income

--- scripts/summary.js
+++ scripts/summary.js
@@ -14,6 +14,12 @@ const summary = {
         sellingStats: () => {
             $('#summary-results').empty();
             $('#summary-results').append(summary.html.itemsContainer);
+            if($('#summary-labels')){$('#summary-labels').remove() }
+            const labels = summary.html.itemLabels();
+            $('#summary-navigation').append(labels.container);
+            $('#summary-labels').append(labels.inputs);
+            $('#summary-labels .quantity').on('click', null, '.quantity', summary.update.orderBy);
+            $('#summary-labels .income').on('click', null, '.income', summary.update.orderBy);
             let firstDay    = $('#day-start').val();
             let lastDay     = $('#day-end').val();
             sendRequest("/summary", {first: firstDay, last: lastDay}, (data) => {
@@ -21,12 +27,17 @@ const summary = {
                     const html = summary.html.itemContainer({id: data.id[i], name: name, quantity: data.count[i], income: data.income[i]});
                     $('#summary-items').append(html.container); 
                     $(`#${data.id[i]}`).append(html.inputs);
+                    $(`#${data.id[i]}`).css('order', data.count[i]);
                 });
             }); 
         },
         hourStats: () => {
             $('#summary-results').empty();
             $('#summary-results').append(summary.html.hoursContainer);
+            if($('#summary-labels')){$('#summary-labels').remove() }
+            const labels = summary.html.hourLabels();
+            $('#summary-navigation').append(labels.container);
+            $('#summary-labels').append(labels.inputs);
             let firstDay    = $('#day-start').val();
             let lastDay     = $('#day-end').val();
             sendRequest("/summary/hours", {first: firstDay, last: lastDay}, (data) => {
@@ -34,11 +45,20 @@ const summary = {
                         const html = summary.html.hourContainer(key, data[key]);
                         $('#summary-hours').append(html.container);
                         $(`#${key}`).append(html.inputs);
+                        
                     }
             });    
         },
     },
-    
+    update: {
+        orderBy: (event) => {
+            const items = $('.stats-item');
+            for(var i=0; i<items.length; i++){
+                let value = 100 * Number(($(`#${items[i].id} ${event.data}`).val()));
+                $(items[i]).css('order', value.toFixed(0));
+            }
+        }
+    },
     manage: {
         getDate: () => {
             const now = new Date();
@@ -65,17 +85,31 @@ const summary = {
             return [dateStartInput, dateEndInput, sellingStatsButton, hoursStatsButton];
         },
         itemContainer: (item) => {
-            const   container       = `<div id='${item.id}'</div>`;
+            const   container       = `<div id='${item.id}' class='stats-item'</div>`;
             const   nameInput       = `<input class='name'     value='${item.name}' readonly>`,
-                    quantityInput   = `<input class='quantity' value='${item.quantity}'' readonly>`,
+                    quantityInput   = `<input class='quantity' value='${item.quantity}' readonly>`,
                     incomeInput     = `<input class='income'   value='${Number(item.income).toFixed(2)}' readonly>`;
             return {container: container, inputs: [nameInput, quantityInput, incomeInput]};
         },
+        itemLabels: () => {
+            const       container = `<div id='summary-labels'></div>`,
+                        nameLabel       = `<input class='name'     value='Nazwa' readonly>`,
+                        quantityLabel   = `<input class='quantity' value='Ilość' readonly>`,
+                        incomeLabel     = `<input class='income'   value='Wpływ' readonly>`;
+            return {container: container, inputs: [nameLabel, quantityLabel, incomeLabel]};
+        },
         hourContainer: (hour, quantity) => {
-            const   hourContainer = `<div id='${hour}'></div>`,
+            const   hourContainer = `<div id='${hour}' class='stats-item'></div>`,
                     hourInput = `<input type='text' value='${hour}:00' readonly>`,
                     quantityInput = `<input type='text' value ='${quantity}' readonly>`;
             return {container: hourContainer, inputs: [hourInput, quantityInput]};
+        },
+        hourLabels: () => {
+            const   container = `<div id='summary-labels'></div>`,
+                    hourLabel = `<input type='text' value='Godzina' readonly>`,
+                    quantityLabel = `<input type='text' value ='Ilość zamówień' readonly>`;
+                    
+            return {container: container, inputs: [hourLabel, quantityLabel]};
         },
     },
 };
